988349bb8579278c6fe7e95923c98e04cee6608b
sooqyemen <aboramez965@gmail.com>
2026-01-03 20:57:00 +0300

    Update page.js

--- app/chat/[id]/page.js
+++ app/chat/[id]/page.js
@@ -1,7 +1,6 @@
 // app/chat/[id]/page.js
 'use client';
 
-import Header from '@/components/Header';
 import ChatBox from '@/components/Chat/ChatBox';
 import Link from 'next/link';
 
@@ -13,20 +12,17 @@ export default function ChatPage({ params, searchParams }) {
   const otherUid = searchParams?.otherUid ? String(searchParams.otherUid) : null;
 
   return (
-    <>
-      <Header />
-      <div className="container">
-        <div className="row" style={{ justifyContent: 'space-between' }}>
-          <Link className="btn" href="/">
-            ← رجوع
-          </Link>
-          <span className="badge">💬 محادثة</span>
-        </div>
-
-        <div style={{ marginTop: 12 }}>
-          <ChatBox chatId={chatId} listingId={listingId} otherUid={otherUid} />
-        </div>
+    <div className="container">
+      <div className="row" style={{ justifyContent: 'space-between' }}>
+        <Link className="btn" href="/">
+          ← رجوع
+        </Link>
+        <span className="badge">💬 محادثة</span>
       </div>
-    </>
+
+      <div style={{ marginTop: 12 }}>
+        <ChatBox chatId={chatId} listingId={listingId} otherUid={otherUid} />
+      </div>
+    </div>
   );
 }
